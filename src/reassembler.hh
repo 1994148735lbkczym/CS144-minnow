a5014920e6d8b81a860214c7d5086da1f796caf3
Raymond <raymond.llata@gmail.com>
2025-01-16 00:59:30 +0000

    Add header file to reassembler to store state

--- src/reassembler.hh
+++ src/reassembler.hh
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "byte_stream.hh"
+#include <unordered_map>
+
 
 class Reassembler
 {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) {}
+  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ), recent_index(-1), saved_bytes() {}
 
   /*
    * Insert a new substring to be reassembled into a ByteStream.
@@ -43,4 +45,8 @@ public:
 
 private:
   ByteStream output_;
+
+  uint64_t recent_index;
+  std::unordered_map<uint64_t, char> saved_bytes;
+
 };
