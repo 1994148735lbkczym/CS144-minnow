642de71e14c42746837378eb3c75917c46b737f6
Raymond <raymond.llata@gmail.com>
2025-01-16 23:57:48 +0000

    Finish ip_raw.cc after successfully sending

--- apps/ip_raw.cc
+++ apps/ip_raw.cc
@@ -1,4 +1,6 @@
 #include "socket.hh"
+#include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -8,7 +10,7 @@ public:
   RawSocket() : DatagramSocket( AF_INET, SOCK_RAW, IPPROTO_RAW ) {}
 };
 
-int main()
+void send_internet_datagram(const string& payload)
 {
   // construct an Internet or user datagram here, and send using the RawSocket as in the Jan. 10 lecture
   string packet;
@@ -18,36 +20,60 @@ int main()
 
   packet += char(64);
   // protocol number 5->Raw, 17->UDP
-  packet += char(5);
-  //packet += char(17);
+  //packet += char(5);
+  packet += char(17);
   packet += string(6, 0);
 
   // 10.144.0.54
   packet += char(10);
   packet += char(144);
   packet += char(0);
-  packet += char(54);
+  packet += char(164);
 
   // UDP INFO
-  // packet += char( 0 );
-  // packet += char( 1 );
+  packet += char( 0 );
+  packet += char( 1 );
 
-  // packet += char( 4 ); // specify dst port 1024 
-  // packet += char( 0 );
+  packet += char( 4 ); // specify dst port 1024 
+  packet += char( 0 );
 
-  // string user_payload = "Hellooo";
+  packet += char ( 0 );
+  packet += char (payload.length() + 8); // package size?
 
-  // packet += char ( 0 );
-  // packet += char (user_payload.length() + 8);
-
-  // packet += string(2, 0);
+  packet += string(2, 0);
 
 
-  packet += "Hellooo";
+  packet += payload;
 
-  printf("sent\n");
+  // printf("sent\n");
 
+  string address = "10.144.0.164";
 
-  RawSocket {}.sendto( Address { "10.144.0.54" }, packet);
-  return 0;
+  RawSocket {}.sendto( Address { address }, packet);
+}
+
+void send_icmp_message( const string& payload )
+{
+  send_internet_datagram( "\x08" + payload );
+}
+
+void program_body()
+{
+  string payload;
+  while ( cin.good() ) {
+	getline( cin, payload );
+	send_icmp_message( payload + "\n" );
+  }
+}
+
+int main()
+{
+  try {
+	program_body();
+  } catch ( const exception& e ) {
+	cerr << e.what() << "\n";
+	return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
