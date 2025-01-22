f228d4cb6ad90995d53724a00132c6ca22fa410f
Raymond <raymond.llata@gmail.com>
2025-01-22 00:17:21 +0000

    Add wrapping wrapping_integers
    Uses the provided helper function to convert checkpoint to a wrapp
    and then calculate the distance from our main object. Using this distance
    create two more wraps, plus/minus the distance from checkpoint, and check which one
    is our original object

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,14 +5,25 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  // ISN + zero_point % 2^32
+  return Wrap32{static_cast<uint32_t>(zero_point.raw_value_ + (n & ((1ULL << 32) - 1)))};;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  // wrap the checkpoint
+  Wrap32 c_point = Wrap32::wrap(checkpoint, zero_point);
+  // abs value logic
+  uint32_t diff = (c_point.raw_value_ - this->raw_value_ < this->raw_value_ - c_point.raw_value_)
+                ? (c_point.raw_value_ - this->raw_value_)
+                : (this->raw_value_ - c_point.raw_value_);
+  
+  uint64_t tmp = checkpoint + diff;
+  uint64_t tmp_sub = (checkpoint < diff) ? ((uint32_t)checkpoint - diff) : checkpoint - diff;
+
+  if (Wrap32::wrap(tmp, zero_point) == *this) 
+    return tmp;
+  if (Wrap32::wrap(tmp_sub, zero_point) == *this) 
+    return tmp_sub;
+  return 0;
 }
