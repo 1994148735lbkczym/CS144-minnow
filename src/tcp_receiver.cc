324e30a7d8395410f0a5821ab4f080b34d915995
Raymond <raymond.llata@gmail.com>
2025-01-23 04:22:36 +0000

    Implement tcp_receiver

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -5,14 +5,56 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+  // check for SYN flag and store ISN properly
+  if (message.SYN && !this->received_isn_) {
+    this->received_isn_ = true;
+    this->zero_point_ = message.seqno;
+    this->first_unassembled_index_++;
+  }
+
+  // set error if RST flag?
+  if (message.RST) {
+    reassembler_.reader().set_error();
+    this->received_rst_ = true;
+    return;
+  }
+
+  // cannot write if no isn; return and (raise error?)
+  if (!this->received_isn_) {
+    return;
+  }
+
+  // calculate first_index by unwrapping message; update if this is the message where we received syn
+  uint64_t first_index = message.seqno.unwrap(this->zero_point_, this->first_unassembled_index_);
+  if (message.SYN && message.payload.size() != 0) {
+    first_index++;
+  }
+  // insert message to reassembler, and update state; -1 since SYN does not count for reassembler
+  uint64_t reassembler_capacity_pre = reassembler_.writer().available_capacity();
+  reassembler_.insert(first_index - 1, message.payload, message.FIN);
+  uint64_t reassembler_capacity_post = reassembler_.writer().available_capacity();
+  this->first_unassembled_index_ += reassembler_capacity_pre - reassembler_capacity_post;
+  
+  // add FIN byte if needed
+  if (reassembler_.writer().is_closed()) {
+    this->first_unassembled_index_++;
+  }
+
+  
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  debug( "unimplemented send() called" );
-  return {};
+  TCPReceiverMessage message;
+  // Add optional ackno if we have recieved an ISN
+  if (this->received_isn_) {
+    message.ackno = Wrap32::wrap(this->first_unassembled_index_, this->zero_point_);
+  }
+  // WINDOWSIZE = MAX_SIZE - USED_INDEXES(not including syn/fin)
+  message.window_size = min((uint64_t)UINT16_MAX, reassembler_.writer().available_capacity());
+
+  // RST is true if bytestream detected error or Receiver had an error packet
+  message.RST = this->reassembler_.writer().has_error() || this->received_rst_;
+
+  return message;
 }
