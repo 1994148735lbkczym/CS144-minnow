06decb7062ae3dc21b2de990eabd91fe901cfce9
Raymond <raymond.llata@gmail.com>
2025-01-30 22:00:58 +0000

    Add preliminary state for tick/recieve/RTO Timer

--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -11,7 +11,7 @@ class TCPSender
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
+    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms ), curr_RTO_ms_( initial_RTO_ms )
   {}
 
   /* Generate an empty TCPSenderMessage */
@@ -29,6 +29,15 @@ public:
   /* Time has passed by the given # of milliseconds since the last time the tick() method was called */
   void tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit );
 
+  /* Set the Retransmission timer alarm, generally references RTO value */
+  void set_rto_alarm();
+
+  /* Check the existing Retransmission timer alarm given time since last tick information, returns true if timer has expired, false otherwise */
+  bool check_rto_alarm( uint64_t ms_since_last_tick );
+
+  /* Stop the RTO alarm if all outstanding data has been acknowledged */
+  void stop_rto_alarm();
+
   // Accessors
   uint64_t sequence_numbers_in_flight() const;  // For testing: how many sequence numbers are outstanding?
   uint64_t consecutive_retransmissions() const; // For testing: how many consecutive retransmissions have happened?
@@ -42,4 +51,15 @@ private:
   ByteStream input_;
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  uint64_t curr_RTO_ms_;
+
+  // state for tcp_sender
+  uint64_t expected_ackno_ = 0;
+  uint64_t last_segment_sent_ = 0;
+  unordered_map<uint64_t, TCPSenderMessage> outstanding_segments = {};
+
+
+  // state(s) for retransimission timer; only used in rto_timer_functions
+  bool rto_timer_is_on_ = false;
+  uint64_t rto_timer_ttl_ = 0;
 };
